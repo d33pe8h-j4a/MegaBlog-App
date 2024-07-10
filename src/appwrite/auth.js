2145b9045c8b92743fdb2fbce883d280f1b91063
Deepesh Jha <deepesh.it@outlook.com>
2024-07-10 21:30:52 +0530

    Completed building the Authentication service

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -41,6 +41,23 @@ export class AuthService {
             throw err;
         }
     }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
 }
 
 const authService = new AuthService();
