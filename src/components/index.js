2389f099f4ccc4f7d3d8d980f4df357d9c73aa6e
Deepesh Jha <deepesh.it@outlook.com>
2024-09-10 19:40:24 +0530

    Added all the components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,9 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-export { Header, Footer };
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+export { Header, Footer, Logo, Container, LogoutBtn, RTE, Login };
